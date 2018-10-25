69213f3ee0a688b484eb58490f0f98296e8a0301
Clark Newell <williamcnewell@gmail.com>
2018-10-24 21:14:01 -0600

    Updates

---
+++ precache-manifest.780992ca71c83c73e72c2ff8ead3ffd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ac01fa0984b51c3cfa9",
+    "url": "/max-react2/static/js/runtime~main.8ac01fa0.js"
+  },
+  {
+    "revision": "6041561d3e3b4b08c1b6",
+    "url": "/max-react2/static/js/main.6041561d.chunk.js"
+  },
+  {
+    "revision": "8ef85dc666309cfc6b09",
+    "url": "/max-react2/static/js/1.8ef85dc6.chunk.js"
+  },
+  {
+    "revision": "6041561d3e3b4b08c1b6",
+    "url": "/max-react2/static/css/main.c8779c55.chunk.css"
+  },
+  {
+    "revision": "358b84adffd17ef5d71106555df7c5b8",
+    "url": "/max-react2/index.html"
+  }
+];
